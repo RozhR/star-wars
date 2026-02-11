db47d9dadcdcf7f4acfe637253a74fb6d23fe9e6
RozhR <rozhroman@gmail.com>
2026-02-11 15:21:26 +0200

    Add initial UI components and layout structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Header from "./components/Header.jsx";
-import Content from "./components/Content.jsx";
+import Main from "./components/Main.jsx";
 import Footer from "./components/Footer.jsx";
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     return (
         <body className="container-fluid">
                 <Header/>
-                <Content/>
+                <Main/>
                 <Footer/>
         </body>
     )
