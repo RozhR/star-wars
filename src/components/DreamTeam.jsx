73203eb6eb4cd56bca4a491547b43ba7ce3bac7a
RozhR <rozhroman@gmail.com>
2026-02-28 12:01:58 +0200

    Refactor AboutMe and Contact components to implement localStorage-based caching for API responses, and add Contact form with styling.

--- src/components/DreamTeam.jsx
+++ src/components/DreamTeam.jsx
@@ -1,11 +1,11 @@
 import Friend from "./Friend.jsx";
-import {dreamTeam} from "../utils/constants.js";
+import {friends} from "../utils/constants.js";
 
 const DreamTeam = () => {
     return (
         <section className="float-end w-50 mt-1 ms-2 me-0 row border border-warning rounded-bottom-5">
             <h2 className="col-sm-12 text-center">Dream team</h2>
-            {dreamTeam.map((title, i) => <Friend friend={title} pos={++i} key={title}/>)}
+            {friends.map((title, i) => <Friend friend={title} pos={++i} key={title}/>)}
         </section>
     );
 };
